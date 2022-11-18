78c8f3467a54c81cbea25d7d363fd063410d2248
Osorabrian <osorabrian@gmail.com>
2022-11-18 18:48:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const { default: normalizeOpts } = require("@babel/core/lib/transformation/normalize-opts");
+
+function writeCards(names, eventName ){
+    const newWriteCards = []
+    for(let n = 0; n<names.length; n++){
+        newWriteCards.push(`Thank you, ${names[n]}, for the wonderful ${eventName} gift!`);
+        debugger;
+    } 
+    return newWriteCards;
+}
+
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday")
+
+function countDown(originalNumber){
+    while (originalNumber>=0){
+        console.log(originalNumber--)
+    }
+}   
+
+countDown(10)
